8d8ccbe6498c1e1159d22c3c1958d846f3cd9bc1
Ali Al-Taweel <engalihaitham@gmail.com>
2025-12-23 17:06:43 +0200

    ex.01 completed

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-def multiplay(x,t):
-    return x*t
+#Exercise 1
+#Write a python program that:#
+
+# 1-Asks the user about their name
+# 2-Save it in a variable called "name"
+# 3-Asks the user about their age
+# 4-Save it in a variable called "age"
+# 5-Print a string stating "The user -name- is -age- years old"
+"""
+name= input("Enter your full name:  ")
+age= input("Enter your age:  ")
+print(type(age))
+print(f'The user {name} is {age} years old')
+
+
+input("Press Enter to exit...")
+"""
+Exercise 2
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message. Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
